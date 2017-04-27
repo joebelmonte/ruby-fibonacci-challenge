09937520e387fe22d1ade8567bea9c166797aa3b
yourUsername <joseph.belmonte@gmail.com>
2017-04-27 17:03:29 -0400

    Answer to challenge

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,7 +1,28 @@
 # frozen_string_literal: true
 
 class FibonacciRange
+  include Enumerable
+
+  def initialize(stop)
+    @stop = stop
+    @current_fib = 0
+    @previous_fib = 0
+    @next_fib = 1
+    @answer = [0]
+  end
+
+  def each
+    while @current_fib < @stop do
+      yield @current_fib
+      @current_fib = @next_fib
+      @next_fib = @current_fib + @previous_fib
+      @previous_fib = @current_fib
+    end
+  end
 end
 
-class FibonacciNumber
-end
+
+# FibonacciRange.new(10)
+
+
+# current_fib, previous_fib, and next_fib
